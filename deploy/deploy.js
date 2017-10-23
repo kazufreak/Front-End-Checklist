0308c0a532b4438af6edf3e510b342943aff426e
miya0001 <miya@wpist.me>
2017-10-23 21:16:29 +0900

    some fix

--- deploy/deploy.js
+++ deploy/deploy.js
@@ -1,21 +1,20 @@
 global.Promise = require('bluebird');
 require('isomorphic-fetch');
-fs = require( 'fs' );
+
+const fs = require( 'fs' );
+const Qiita = require( 'qiita-js' );
 
 if ( "false" !== process.env.TRAVIS_PULL_REQUEST ) {
   console.log( 'Thanks for contribution. :)' );
   process.exit();
 }
 
-// require in commonjs env
-var Qiita = require( 'qiita-js' );
-
 // set your token
 Qiita.setToken( process.env.QIITA );
 Qiita.setEndpoint( 'https://qiita.com' );
 
-fs.readFile( __dirname + '/qiita.md', 'utf8', function ( err, head ) {
-  fs.readFile( 'README.md', 'utf8', function ( err, readme ) {
+fs.readFile( __dirname + '/qiita.md', 'utf8', ( err, head ) => {
+  fs.readFile( 'README.md', 'utf8', ( err, readme ) => {
     var contents = head + "\n---\n" + readme;
     contents = contents.replace( /<\!\-\-[\s\S]*?\-\->/g, '' );
     Qiita.Resources.Item.update_item( '8fff46c201bf9eaeba4a', {
