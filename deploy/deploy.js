1ae9dff74043c31eee4a49e0e2e8d0d189f378a8
miya0001 <miya@wpist.me>
2017-10-24 15:47:29 +0900

    update deploy scripts

--- deploy/deploy.js
+++ deploy/deploy.js
@@ -10,15 +10,15 @@ if ( "false" !== process.env.TRAVIS_PULL_REQUEST ) {
 }
 
 // set your token
-Qiita.setToken( process.env.QIITA );
+Qiita.setToken( process.env.QIITA_TOKEN );
 Qiita.setEndpoint( 'https://qiita.com' );
 
 fs.readFile( __dirname + '/qiita.md', 'utf8', ( err, head ) => {
   fs.readFile( 'README.md', 'utf8', ( err, readme ) => {
     var contents = head + "\n" + readme;
     contents = contents.replace( /<\!\-\-[\s\S]*?\-\->/g, '' );
-    Qiita.Resources.Item.update_item( '8fff46c201bf9eaeba4a', {
-      title: 'フロントエンドチェックリスト（日本語訳）',
+    Qiita.Resources.Item.update_item( process.env.QIITA_POST_ID, {
+      title: process.env.QIITA_POST_TITLE,
       body: contents,
       private: false,
     } ).then( function( res ) {
