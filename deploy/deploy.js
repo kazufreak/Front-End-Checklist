af454e1f509bd4b5dbf0d35d1d7b317dff9adaaf
miya0001 <miya@wpist.me>
2017-10-24 16:27:59 +0900

    add last updated

--- deploy/deploy.js
+++ deploy/deploy.js
@@ -3,6 +3,7 @@ require('isomorphic-fetch');
 
 const fs = require( 'fs' );
 const Qiita = require( 'qiita-js' );
+const moment = require( 'moment-timezone' );
 
 if ( "false" !== process.env.TRAVIS_PULL_REQUEST ) {
   console.log( 'Thanks for contribution. :)' );
@@ -14,6 +15,8 @@ Qiita.setToken( process.env.QIITA_TOKEN );
 Qiita.setEndpoint( 'https://qiita.com' );
 
 fs.readFile( __dirname + '/qiita.md', 'utf8', ( err, head ) => {
+  const now = moment().tz( 'Asia/Tokyo' ).format( 'YYYY-MM-DD HH:mm:ssZ' );
+  head = head.replace( "__LAST_UPDATE__", now );
   fs.readFile( 'README.md', 'utf8', ( err, readme ) => {
     var contents = head + "\n" + readme;
     contents = contents.replace( /<\!\-\-[\s\S]*?\-\->/g, '' );
